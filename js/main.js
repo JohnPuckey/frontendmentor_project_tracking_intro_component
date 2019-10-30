0f413a48882003dda557c9ac1bb2f07645f23e55
John <johnpuckeydesign@hotmail.com>
2019-10-30 09:39:04 +0000

    fixed nav line, adjusted responsiveness, added animation

--- js/main.js
+++ js/main.js
@@ -10,21 +10,19 @@ menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if(!showMenu) {
-        menuBtn.classList.add('close')
-        menuNav.classList.add('show')
+        menuBtn.classList.add('close')  // Change hamburger into X
+        menuNav.classList.add('show')  // Show hidden Nav Links
 
 
         showMenu = true; // Set menu state
 
 
     } else {
-        menuBtn.classList.remove('close')
-        menuNav.classList.remove('show')
+        menuBtn.classList.remove('close')  // Change X into hamburger
+        menuNav.classList.remove('show')   // Hide Nav Links
 
 
         showMenu = false // Set menu state
 
     }
 }
-
-console.log("hello")
